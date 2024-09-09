219bfde9c1ae48668e781fd2a1c8bc91ff8a213a
Jonasm22 <john.2114@hotmail.com>
2024-09-09 02:01:31 +0200

    Range Added

--- BlazorAppNr1/obj/Debug/net7.0/BlazorAppNr1.AssemblyInfo.cs
+++ BlazorAppNr1/obj/Debug/net7.0/BlazorAppNr1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppNr1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a23a73f17e72d9a23036b92aae8821573d80409")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppNr1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppNr1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
